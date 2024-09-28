1aaf7a93d7de04c5732c12f75131c71bade1911c
MichaelK <96792007+MajklKra@users.noreply.github.com>
2024-09-28 14:09:57 +0200

    Commit 28. 9. 2024

--- src/main/java/cz/vsb/Main.java
+++ src/main/java/cz/vsb/Main.java
@@ -1,5 +1,6 @@
 package cz.vsb;
 
+/*
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,3 +15,24 @@ public class Main {
 
         }
     }
+*/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder)
+    {
+        return applicationBuilder.sources(Main.class);
+    }
+
+}
